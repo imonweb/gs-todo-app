9bb41f5ad233ab87be7f1eeb357dbe3524974258
Imon <contact@imonweb.co.uk>
2024-06-20 16:33:04 +0100

    scaffolding

---
+++ src/components/Todo.jsx
@@ -0,0 +1,44 @@
+import React, { useRef } from 'react'
+import todo_icon from '../assets/todo_icon.png';
+import TodoItems from './TodoItems';
+
+const Todo = () => {
+
+  const inputRef = useRef();
+
+  const add = () => {
+    const inputText = inputRef.current.value.trim();
+    console.log(inputText);
+  }
+
+  return (
+    <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
+      
+      {/* ------- title -------  */}
+
+      <div className="flex items-center mt-7 gap-2">
+        <img className='w-8' src={todo_icon} alt="" />
+        <h1 className='text-3xl font-semibold'>To-Do List</h1>
+      </div>
+
+      {/* ------- input box -------  */}
+
+      <div className='flex intems-center my-7 bg-gray-200 rounded-full'>
+        <input ref={inputRef} className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600" type="text" placeholder='Add your task' />
+        <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg fount-medium cursor-pointer'>ADD +</button>
+      </div>
+
+      {/* ------- todo list -------  */}
+
+      <div>
+        <TodoItems text="Learn Coding" />
+        <TodoItems text="Learn Coding from Imon" />
+      </div>
+
+
+      
+    </div>
+  )
+}
+
+export default Todo
